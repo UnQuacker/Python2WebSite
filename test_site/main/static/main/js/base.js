f3b92dc854774ecda4c5a8769009181e5a7e4811
Talgat Sovetbekov <sarkytbayevquacker@gmail.com>
2022-04-19 09:32:24 +0600

    JavaScript

---
+++ test_site/main/static/main/js/base.js
@@ -0,0 +1,7 @@
+function toggle(){
+    if ($("div.toggling").class("display")==="none"){
+        $("div.toggling").css("display","contents")
+    }else{
+        $("div.toggling").css("display","none")
+    }
+}
